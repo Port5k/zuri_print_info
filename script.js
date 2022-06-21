ed596ca23e23c751e7a78ddaec108ace3fd15a9c
Port5k <port5kinc@gmail.com>
2022-06-21 23:41:29 +0100

    Task completed

---
+++ script.js
@@ -0,0 +1,5 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+
+console.log("Name: Adekunle Adedokun")
+console.log("Height: 173cm")
+console.log("Country: Nigeria")
